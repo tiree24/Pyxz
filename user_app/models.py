4e0b4cc55f1c0a82b01da6c8641946c13d3ba9ab
Timothy Reynoso <domex2467@gmail.com>
2021-01-05 13:13:44 -0500

    created myuser modal

--- user_app/models.py
+++ user_app/models.py
@@ -1,10 +1,19 @@
-from django.db import models
+from django.db import models, 
+
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class MyUser(models.Model):
-    username = models
-    password = models
-    email = models
-    following = models
-    websiteURL = models
+class MyUser(AbstractUser):
+    following = models.ManyToManyField('self', related_name='follow')
+    websiteURL = models.URLField()
+
+    def __str__(self):
+        return self.username
+
+    def get_following(self):
+        return ",".join([str(p) for p in self.following.all()])
+
+    def get_following_num(self):
+        return self.following.count()
+
