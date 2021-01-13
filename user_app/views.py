9d3e89f140b4ef400d9b6ef5bb76ae4353656516
Timothy Reynoso <domex2467@gmail.com>
2021-01-13 11:39:34 -0500

    class OrderedView in the user_app.view was adjusted to only display photos which are not stories

--- user_app/views.py
+++ user_app/views.py
@@ -48,7 +48,7 @@ class OrderedView(View):
 
     def get(self, request, order_by):
         comments = Comment.objects.all()
-        img_set = Image.objects.order_by(order_by)[::-1]
+        img_set = Image.objects.filter(is_story=False).all().order_by(order_by)[::-1]
         stories = Image.objects.filter(is_story=True).all()
         return render(request, self.html, {'img_set': img_set, 'comments': comments, 'form': self.form, 'stories':stories   })
 
