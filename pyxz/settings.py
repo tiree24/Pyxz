ce3d61b7a03fcaab9133d19f66b82c7861cb1d7c
Marcus Chiriboga <marcuschiriboga@gmail.com>
2021-01-16 13:50:47 -0500

    forgot to save settings.py

--- pyxz/settings.py
+++ pyxz/settings.py
@@ -135,5 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 AUTH_USER_MODEL = "user_app.MyUser"
