3bbdddad1e7580dce9ed704d30f60a849bdbeaa6
Marcus Chiriboga <marcuschiriboga@gmail.com>
2021-01-17 16:33:05 -0500

    superuser

--- user_app/forms.py
+++ user_app/forms.py
@@ -16,7 +16,6 @@ class SignUpForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     profile_pyxz = forms.ImageField()
-    is_superuser = forms.BooleanField(default=True)
 
 class LoginForm(forms.Form):
 
