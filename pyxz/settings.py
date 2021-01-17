4b7385b09ab813a37c8a01d59a312ffad919257e
Marcus Chiriboga <marcuschiriboga@gmail.com>
2021-01-17 15:04:08 -0500

    /

--- pyxz/settings.py
+++ pyxz/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/i")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/i/")
 AUTH_USER_MODEL = "user_app.MyUser"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
