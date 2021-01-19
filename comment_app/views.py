58aa56aa02915e6268bc6166a6537926ecda0c22
Marcus Chiriboga <marcuschiriboga@gmail.com>
2021-01-19 12:54:00 -0500

    redirecting secret key

--- comment_app/views.py
+++ comment_app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from comment_app.models import Comment
 from user_app.models import MyUser
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-
+@login_required
 def CommentLikeUpView(request, comment_id):
     target = Comment.objects.get(id=comment_id)
     auth_user = MyUser.objects.get(id=request.user.id)
     target.likes.add(auth_user)
     target.save()
     return redirect(request.META.get('HTTP_REFERER'))
-
+@login_required
 def CommentLikeDownView(request, comment_id):
     target = Comment.objects.get(id=comment_id)
     auth_user = MyUser.objects.get(id=request.user.id)
