14105d874a4abafedcdfc93437598b077b76fbce
Tracy DeWitt <Justdewitt8485@gmail.com>
2021-01-19 15:15:34 -0600

    added changes

--- pyxz/settings.py
+++ pyxz/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from local_settings import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from local_settings import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- pyxz/urls.py
+++ pyxz/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from auth_app.views import LoginFormView, LogoutView
 from comment_app.views import CommentLikeDownView, CommentLikeUpView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,17 +24,17 @@ from photo_app.views import (AllTags, Image_view, ImageUpload, LikeDownView,
 from user_app.views import (EditFormView, FollowTagsView, FollowUserView,
                             FollowView, HomePage, OrderedView, Profile,
                             SearchView, SignUp, TopView, UnFollowView,
-                            UserFollowers, UsersPageView, funcView)
+                            UserFollowers, UsersPageView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePage.as_view(), name='All'),
-    path('order/<str:order_by>/', OrderedView.as_view(), name='Ordered'),
+    path('', HomePage.as_view(), name ='All'),
+    path('order/<str:order_by>/', OrderedView.as_view(), name ='Ordered'),
     path('top/', TopView.as_view(), name='Top'),
     path("following/", FollowUserView.as_view()),
     path("followingtags/", FollowTagsView.as_view()),
     path('whoifollow/<int:user_id>/', UserFollowers.as_view()),
-    path('profile/<int:user_id>/', Profile.as_view(), name = 'Profile'),
+    path('profile/<int:user_id>/', Profile.as_view(), name ='Profile'),
     path('profile/edit', EditFormView),
     path('signup/', SignUp.as_view(), name='Signup'),
     path('listoftags/', AllTags.as_view(), name='Tags'),
@@ -47,7 +48,7 @@ urlpatterns = [
     path("likedown/<int:img_id>/", LikeDownView),
     path("follow/<int:user_id>/", FollowView),
     path("unfollow/<int:user_id>/", UnFollowView),
-    path("commentlikeup/<int:comment_id>/", CommentLikeUpView),
+    path("commentlikeup/<int:comment_id>/", CommentLikeUpView), 
     path("commentlikedown/<int:comment_id>/", CommentLikeDownView),
     path("search/", SearchView.as_view(), name='search'),
     path("users/", UsersPageView.as_view(), name='users'),
